304aea174d7eff252df1c8184f704d089f444406
GarrikDanilov <garrik_danilov@mail.ru>
2022-10-12 03:41:23 +0300

    update 1

--- 8/app.js
+++ 8/app.js
@@ -43,12 +43,25 @@ class Product {
         this.totalPrice += this.price;
     }
 
-    getProductMarkup() { }
+    getProductMarkup() {
+        return `
+        <tr>
+            <td>${this.productName}</td>
+            <td>${this.count} шт.</td>
+            <td>$${this.price}</td>
+            <td>$${this.totalPrice}</td>
+        </tr>
+        `;
+    }
 }
 
 class UserCart {
-    constructor() {
+    constructor(baseDiv) {
         this.products = [];
+        this.totalPrice = this.getTotalPrice();
+        baseDiv.insertAdjacentHTML("beforeend", `${this.getUserCartMarkup()}`);
+        this.tableObj = baseDiv.querySelector('.cart__table');
+        this.totalPriceObj = this.tableObj.querySelector('.total__price span');
     }
 
     qetTotalCount() {
@@ -57,7 +70,58 @@ class UserCart {
         }
 
         totalCount = 0;
-        this.products.forEach(item => this.totalCount += item.count);
+        this.products.forEach(item => totalCount += item.count);
         return totalCount;
     }
+
+    getTotalPrice() {
+        if (this.products.length === 0) {
+            return 0;
+        }
+
+        totalPrice = 0;
+        this.products.forEach(item => totalPrice += item.totalPrice);
+        return totalPrice;
+    }
+
+    /**
+     * 
+     * @param {Product} product 
+     */
+    updateTotalPrice(product) {
+        this.totalPrice += product.totalPrice;
+    }
+
+    getUserCartMarkup() {
+        return `
+        <table class="cart__table">
+        <caption class="total__price">Товаров в корзине на сумму: <span>$${this.totalPrice}</span></caption>
+        <tr>
+            <th>Название товара</th>
+            <th>Количество</th>
+            <th>Цена за шт.</th>
+            <th>Итого</th>
+        </tr>
+        </table>
+        `;
+    }
+
+    /**
+     * 
+     * @param {Product} product 
+     */
+    addProduct(product) {
+        this.products.push(product);
+        this.updateTotalPrice(product);
+        this.totalPriceObj.textContent = `$${this.totalPrice}`;
+        this.tableObj.insertAdjacentHTML("beforeend", `${product.getProductMarkup()}`);
+    }
 }
+
+const userCartBase = document.querySelector('.rightHeader');
+const newCart = new UserCart(userCartBase);
+
+const prod1 = new Product('product 1', 100);
+newCart.addProduct(prod1);
+const prod2 = new Product('product 2', 50);
+newCart.addProduct(prod2);
